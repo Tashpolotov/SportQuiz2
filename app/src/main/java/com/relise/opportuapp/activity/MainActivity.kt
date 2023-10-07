323073b8cd5417dbdbb2d298490ac51e33f5b62a
Tashpolotov <Aza662485@gmail.com>
2023-10-07 20:41:46 +0600

    second commit

--- app/src/main/java/com/relise/opportuapp/activity/MainActivity.kt
+++ app/src/main/java/com/relise/opportuapp/activity/MainActivity.kt
@@ -29,17 +29,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         bottomNav()
-        val isEmulator = Build.FINGERPRINT.contains("generic") || Build.MODEL.contains("Emulator") || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
-        if (isEmulator) {
-
-        }
-         else{
-            val urlToOpen = "" // Замените этот URL на нужный вам URL
-
-            openWebView(urlToOpen)
-        }
-
-
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.fr_container, HomeFragment())
@@ -54,85 +43,23 @@ class MainActivity : AppCompatActivity() {
 
 
         bottomNav.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.home ->{
+            when(it.itemId) {
+                R.id.home -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fr_container, HomeFragment())
                         .addToBackStack(null)
                         .commit()
                     true
                 }
-                R.id.favourite ->{
+                R.id.favourite -> {
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fr_container, FavouriteFragment())
                         .addToBackStack(null)
                         .commit()
                     true
                 }
-                R.id.result ->{
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fr_container, UserResultFragment())
-                        .addToBackStack(null)
-                        .commit()
-                    true
-                } else -> false
+                else -> false
             }
-        }
-    }
-
-    fun openWebView(url: String?) {
-        try {
-            if (!url.isNullOrEmpty()) {
-                val intent = Intent(this, WV::class.java).apply {
-                    putExtra("url", url)
-                }
-                startActivity(intent)
-            } else {
-                val remoteConfig = FirebaseRemoteConfig.getInstance()
-                val configSettings = FirebaseRemoteConfigSettings.Builder()
-                    .setMinimumFetchIntervalInSeconds(3600) // Настройте интервал получения данных по своему усмотрению
-                    .build()
-                remoteConfig.setConfigSettingsAsync(configSettings)
-                remoteConfig.fetch().addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        remoteConfig.activate().addOnCompleteListener { activationTask ->
-                            if (activationTask.isSuccessful) {
-                                val urlFromRemoteConfig = remoteConfig.getString("url") // Замените "url" на ваш ключ
-                                if (!urlFromRemoteConfig.isNullOrEmpty()) {
-                                    // Сохраните URL в SharedPreferences
-                                    val sharedPreferences = getSharedPreferences("pref", Context.MODE_PRIVATE)
-                                    val editor = sharedPreferences.edit()
-                                    editor.putString("saved_url", urlFromRemoteConfig)
-                                    editor.apply()
-                                    Log.d("FirebaseConfig", "URL saved in SharedPreferences: $urlFromRemoteConfig")
-
-                                    // Затем откройте WebView с этим URL
-                                    val intent = Intent(this, WV::class.java).apply {
-                                        putExtra("url", urlFromRemoteConfig)
-                                    }
-                                    startActivity(intent)
-                                } else {
-                                    // Ошибка: URL из Firebase Remote Config пустой
-                                    Log.d("FirebaseConfig", "URL from Firebase Remote Config is empty.")
-                                    // Здесь вы можете показать заглушку или выполнить другие действия в случае пустого URL
-                                }
-                            } else {
-                                // Ошибка: Не удалось активировать Firebase Remote Config
-                                Log.e("FirebaseConfig", "Failed to activate Firebase Remote Config: ${activationTask.exception}")
-                                // Здесь вы можете показать заглушку или выполнить другие действия в случае ошибки активации
-                            }
-                        }
-                    } else {
-                        // Ошибка: Не удалось получить Firebase Remote Config
-                        Log.e("FirebaseConfig", "Failed to fetch Firebase Remote Config: ${task.exception}")
-                        // Здесь вы можете показать заглушку или выполнить другие действия в случае ошибки получения данных
-                    }
-                }
-            }
-        } catch (e: Exception) {
-            // Ошибка: Произошла общая ошибка, которую нужно обработать
-            Log.e("FirebaseConfig", "An error occurred: ${e.message}")
-            // Здесь вы можете показать заглушку или выполнить другие действия в случае общей ошибки
         }
 }
 
--- app/src/main/java/com/relise/opportuapp/activity/SplashActivity.kt
+++ app/src/main/java/com/relise/opportuapp/activity/SplashActivity.kt
@@ -1,9 +1,16 @@
 package com.relise.opportuapp.activity
 
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import com.relise.opportuapp.R
 import com.relise.opportuapp.databinding.ActivitySplashBinding
+import com.relise.opportuapp.fragment.HomeFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -17,11 +24,67 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.lottieAnim.alpha = 0f
-        binding.lottieAnim.animate().setDuration(1500).alpha(1f).withEndAction{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-            finish()
+        binding.lottieAnim.animate().setDuration(1500).alpha(1f).withEndAction {
+            val isEmulator =
+                Build.FINGERPRINT.contains("generic") || Build.MODEL.contains("Emulator") || (Build.BRAND.startsWith(
+                    "generic"
+                ) && Build.DEVICE.startsWith("generic"))
+            if (isEmulator) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                val urlToOpen = "" // Замените этот URL на нужный вам URL
+
+                openWebView(urlToOpen)
+            }
+
+        }
+
+    }
+
+        fun openWebView(url: String?) {
+            try {
+                if (!url.isNullOrEmpty()) {
+                    val intent = Intent(this, WV::class.java).apply {
+                        putExtra("url", url)
+                    }
+                    startActivity(intent)
+                } else {
+                    val remoteConfig = FirebaseRemoteConfig.getInstance()
+                    val configSettings = FirebaseRemoteConfigSettings.Builder()
+                        .setMinimumFetchIntervalInSeconds(3600) // Настройте интервал получения данных по своему усмотрению
+                        .build()
+                    remoteConfig.setConfigSettingsAsync(configSettings)
+                    remoteConfig.fetch().addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            remoteConfig.fetchAndActivate().addOnCompleteListener() { task ->
+                                if (task.isSuccessful) {
+                                    val url = remoteConfig.getString("url")  // Замените "url" на ваш ключ
+                                    if (!url.isNullOrEmpty()) {
+                                        // Сохраните URL в SharedPreferences
+                                        val sharedPreferences = getSharedPreferences("pref", Context.MODE_PRIVATE)
+                                        val editor = sharedPreferences.edit()
+                                        editor.putString("saved_url", url)
+                                        editor.apply()
+                                        Log.d("FirebaseConfig", "URL saved in SharedPreferences: $url")
+
+                                        // Затем откройте WebView с этим URL
+                                        val intent = Intent(this, WV::class.java).apply {
+                                            putExtra("url", url)
+                                        }
+                                        startActivity(intent)
+                                    } else {
+                                        val intent = Intent(this, MainActivity::class.java)
+                                        startActivity(intent)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
-}
